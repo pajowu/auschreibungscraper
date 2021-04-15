b6ac3feddbb9e2fb370e7735f518f747e56c8e10
pajowu <git@ca.pajowu.de>
2021-04-15 22:25:41 +0200

    Vergabekooperation: Start new cookiejar for each Details run

--- auschreibungscraper/spiders/vergabekooperation.py
+++ auschreibungscraper/spiders/vergabekooperation.py
@@ -10,9 +10,9 @@ class VergabekooperationSpider(scrapy.Spider):
 
     th_contexts = ["preinformations", "publications", "awardPublications"]
     start_urls = [
-        "https://vergabekooperation.berlin/NetServer/PublicationSearchControllerServlet?Max=1&function=SearchPublications&Gesetzesgrundlage=All&Category=PriorInformation&thContext=preinformations",
-        "https://vergabekooperation.berlin/NetServer/PublicationSearchControllerServlet?Max=1&function=SearchPublications&Gesetzesgrundlage=All&Category=InvitationToTender&thContext=publications",
-        "https://vergabekooperation.berlin/NetServer/PublicationSearchControllerServlet?Max=1&function=SearchPublications&Gesetzesgrundlage=All&Category=ContractAward&thContext=awardPublications",
+        "https://vergabekooperation.berlin/NetServer/PublicationSearchControllerServlet?Max=10000&function=SearchPublications&Gesetzesgrundlage=All&Category=PriorInformation&thContext=preinformations",
+        "https://vergabekooperation.berlin/NetServer/PublicationSearchControllerServlet?Max=10000&function=SearchPublications&Gesetzesgrundlage=All&Category=InvitationToTender&thContext=publications",
+        "https://vergabekooperation.berlin/NetServer/PublicationSearchControllerServlet?Max=10000&function=SearchPublications&Gesetzesgrundlage=All&Category=ContractAward&thContext=awardPublications",
     ]
 
     def parse(self, response):
@@ -27,10 +27,10 @@ class VergabekooperationSpider(scrapy.Spider):
     def parse_detail(self, response):
         download_path = response.css(".downloadDocuments").css("a").xpath("@href").get()
         download_url = response.urljoin(download_path)
-        yield response.follow(download_url, callback=self.parse_detail_2)
+        yield response.follow(download_url, callback=self.parse_detail_2, meta={'cookiejar': download_path})
 
         publication_tables = response.css(".tableContractNotice")
-        publication_data = {None: {"attachment_download_url": download_url}}
+        publication_data = {None: {"attachment_download_url": download_url}, "__type": "Detail"}
         for publication_table in publication_tables:
             publication_section = publication_table.css(".color-main::text").get()
             if not publication_section in publication_data:
@@ -46,7 +46,8 @@ class VergabekooperationSpider(scrapy.Spider):
         yield publication_data
 
     def parse_detail_2(self, response):
-        data = {"attachment_versions": [], "messages": []}
+        cookiejar = response.meta['cookiejar']
+        data = {"attachment_versions": [], "messages": [], "__type": "Attachement_Messages"}
         for download_version in response.css(".zipFileContents"):
             oid = download_version.xpath("@data-oid").get()
             token = download_version.xpath("@data-token").get()
@@ -58,7 +59,7 @@ class VergabekooperationSpider(scrapy.Spider):
             yield scrapy.http.FormRequest(
                 "https://vergabekooperation.berlin/NetServer/DataProvider",
                 formdata={"param": "FileTree", "oid": oid, "VALIDATION_TOKEN": token},
-                meta={"documentOID": oid},
+                meta={"documentOID": oid, "respon": response, "cookiejar": cookiejar},
                 callback=self.parse_dataprovider_filetree,
             )
 
@@ -78,13 +79,17 @@ class VergabekooperationSpider(scrapy.Spider):
             )
 
     def parse_dataprovider_filetree(self, response):
+        # cookiejar = response.meta['cookiejar']
+        if not response.text:
+            import pdb;pdb.set_trace()
+        print("Response Text", response.text)
         response_json = response.json()
         file_urls = []
         for section_name, section in response_json.items():
             for idx, file in section.items():
                 url = f"https://vergabekooperation.berlin/NetServer/TenderingProcedureDetails?function=_DownloadTenderDocument&documentOID={response.meta['documentOID']}&Document={file['encodedName']}"
                 file_urls.append(url)
-        yield {"FileTree": response_json, "file_urls": file_urls}
+        yield {"FileTree": response_json, "file_urls": file_urls, "__type": "FileTree"}
 
     def parse_dataprovider_publicmessagedetail(self, response):
         # TODO
